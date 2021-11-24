2fcbc947a06f7e5ada63d915b149c81793e4d1cf
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2021-11-24 22:46:39 +0100

    updated variable naming in Ship.cs to fit the swap from boat to ship

--- ShipSimProject/Assets/Main/Scripts/Ship.cs
+++ ShipSimProject/Assets/Main/Scripts/Ship.cs
@@ -13,13 +13,13 @@ public class Ship : MonoBehaviour, IShip
     /// <summary>
     /// Builds a boat on the GameObject this script is bound to.
     /// </summary>
-    /// <param name="boatFactory">The BoatFactory object to build the boat from</param>
-    public void Build(IShipFactory boatFactory)
+    /// <param name="shipFactory">The ShipFactory object to build the boat from</param>
+    public void Build(IShipFactory shipFactory)
     {
         // Create and render mesh
         MeshFilter mf = gameObject.AddComponent<MeshFilter>();
         MeshRenderer mr = gameObject.AddComponent<MeshRenderer>();
-        mf.mesh = boatFactory.CreateMesh();
+        mf.mesh = shipFactory.CreateMesh();
 
         // Add rigid body.
         rb = gameObject.AddComponent<Rigidbody>();
@@ -29,15 +29,15 @@ public class Ship : MonoBehaviour, IShip
         detectedEntitiesList = new List<IDetectableEntity>();
 
         // Build properties from boat factory.
-        AccelerationCurve = boatFactory.CreateAccelerationCurve(); 
-        TurningSpeedCurve = boatFactory.CreateTurningSpeedCurve();
-        OptimalTurnSpeed = boatFactory.CreateOptimalTurnSpeed();
-        mr.sharedMaterial = boatFactory.CreateMaterial();
+        AccelerationCurve = shipFactory.CreateAccelerationCurve(); 
+        TurningSpeedCurve = shipFactory.CreateTurningSpeedCurve();
+        OptimalTurnSpeed = shipFactory.CreateOptimalTurnSpeed();
+        mr.sharedMaterial = shipFactory.CreateMaterial();
         
         // Add the 'visual' sphere of the boat. I.e the detection collider
         SphereCollider detectCol = gameObject.AddComponent<SphereCollider>();
         detectCol.isTrigger = true;
-        detectCol.radius = boatFactory.CreateDetectionRange();
+        detectCol.radius = shipFactory.CreateDetectionRange();
     }
 
    
